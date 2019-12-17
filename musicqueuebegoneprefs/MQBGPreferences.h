11b9be76a565d018079d38c9e0189a668f3fcdd3
nahtedetihw <41249541+nahtedetihw@users.noreply.github.com>
2019-12-16 22:27:22 -0600

    Add files via upload

---
+++ musicqueuebegoneprefs/MQBGPreferences.h
@@ -0,0 +1,22 @@
+#import <Preferences/PSListController.h>
+#import <Preferences/PSSpecifier.h>
+#import <CepheiPrefs/HBRootListController.h>
+#import <CepheiPrefs/HBAppearanceSettings.h>
+#import <spawn.h>
+
+@interface MQBGPreferencesListController : HBRootListController {
+
+    UITableView * _table;
+
+}
+@property (nonatomic, retain) UIBarButtonItem *killButton;
+@property (nonatomic, retain) UIView *headerView;
+@property (nonatomic, retain) UIImageView *headerImageView;
+@property (nonatomic, retain) UILabel *titleLabel;
+@property (nonatomic, retain) UIImageView *iconView;
+@property (nonatomic, retain) NSArray *versionArray;
+@end
+
+@interface MQBGAppearanceSettings: HBAppearanceSettings
+
+@end
